0b840fd29137606f0d049bb9deb5b6f7fa6dcab4
aajlil <aymanajlil6@gmail.com>
2026-06-09 16:19:58 +0200

    correction bug affichage

--- public/inscription.js
+++ public/inscription.js
@@ -3,6 +3,10 @@ const registerForm = document.getElementById("registerForm");
 if (registerForm) {
     registerForm.addEventListener("submit", async (e) => {
         e.preventDefault();
+        const btn = document.getElementById("btnInscription");
+        if (btn) {
+            btn.disabled = true;
+        }
         const identifiant = document.getElementById("identifiant").value;
         const nom = document.getElementById("nom").value;
         const prenom = document.getElementById("prenom").value;
@@ -15,9 +19,10 @@ if (registerForm) {
             },
             body: JSON.stringify({nom, prenom, identifiant, email, mdp})
         });
-
         const data = await res.json();
-        const message = document.getElementById("message");
-        message.textContent = data.message;
+        document.getElementById("message").textContent = data.message;
+        if (btn) {
+            btn.disabled = false;
+        }
     });
 }
